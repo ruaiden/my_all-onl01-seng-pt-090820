d8293dfcddff847aec2fcb5f6c0b0890fbb866f2
Aiden Marques <aiden.marques94@gmail.com>
2020-09-20 04:36:45 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -6,7 +6,7 @@ def my_all?(collection)
   
   i = 0 
   while i < collection.length
-  block_return_values << yield(collection[i]
+  block_return_values << yield(collection[i])
   i += 1 
   end
   
